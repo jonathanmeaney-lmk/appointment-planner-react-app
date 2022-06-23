52faca2cb7c10b152b4e2f612bca53c44e23f32a
Jonathan <jonathanmeaney1982@gmail.com>
2022-06-23 10:10:17 +0000

    Added form and tilelist

--- appointment-planner/src/components/contactForm/ContactForm.js
+++ appointment-planner/src/components/contactForm/ContactForm.js
@@ -10,6 +10,43 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    ContactForm
+    <form onSubmit={handleSubmit}>
+      <label>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+          placeholder="Contact Name"
+        />
+      </label>
+      <br />
+      <label>
+        <input
+          type="tel"
+          name="phone"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+          required
+          // regex is for Spain phone numbers
+          pattern="^[0-9]{2,3}-? ?[0-9]{6,7}$"
+          placeholder="Contact Phone"
+        />
+      </label>
+      <br />
+      <label>
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+          placeholder="Contact Email"
+        />
+      </label>
+      <br />
+      <input type="submit" value="Add Contact" />
+    </form>
   );
 };
